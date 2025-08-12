aee8588a6f692ed8b30cf5542a216fad577d858a
Tommy Duong <tommyduong588@gmail.com>
2025-08-11 17:44:50 -0700

    8/11 final push, small changes, preparing for quiz generation

--- model_loader.py
+++ model_loader.py
@@ -3,7 +3,7 @@ from llama_cpp import Llama
 def generate_until_stop(llm, prompt, stop_token=None, max_tokens=300):
     assert isinstance(llm, Llama), "must be a llama model"
     if stop_token is None: #give basic end token
-        stop_token = ""
+        stop_token = [""]
     
     output = ""
     for response in llm(prompt, max_tokens=max_tokens, stream=True): #iterate over all streamed tokens
@@ -16,9 +16,13 @@ def generate_until_stop(llm, prompt, stop_token=None, max_tokens=300):
     return output
 
 
-model_path = "model\mistral-7b-instruct-v0.1-q4_k_m.gguf"
+model_path = r"model\mistral-7b-instruct-v0.1-q4_k_m.gguf" 
 
-llm = Llama(model_path=model_path, n_thread=8) #adjust for cpu cores
+llm = Llama(model_path=model_path, n_thread=8, verbose=False) #adjust for cpu cores, change as needed
 
-prompt = "Explain photosynthesis in relation to how it is a complex process."
-response = generate_until_stop(llm, prompt) 
+prompt = "Explain photosynthesis in relation to how it is a complex process." #sample prompt
+response = generate_until_stop(llm, prompt) #generate and print to terminal
+
+def generate_quiz(llm, topic, num_questions=5):
+    raise NotImplementedError
+    return
