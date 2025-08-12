9d5bb20959837e686eeace90b11a270d71a30fb1
Tommy Duong <tommyduong588@gmail.com>
2025-08-12 13:31:27 -0700

    enabled flash attention, created base quiz generator with new prompt

--- model_loader.py
+++ model_loader.py
@@ -1,6 +1,7 @@
 from llama_cpp import Llama
+from random import randint
 
-def generate_until_stop(llm, prompt, stop_token=None, max_tokens=300):
+def generate_until_stop(llm, prompt, stop_token=None, max_tokens=300): #basic generation without prompt engineering
     assert isinstance(llm, Llama), "must be a llama model"
     if stop_token is None: #give basic end token
         stop_token = [""]
@@ -18,11 +19,35 @@ def generate_until_stop(llm, prompt, stop_token=None, max_tokens=300):
 
 model_path = r"model\mistral-7b-instruct-v0.1-q4_k_m.gguf" 
 
-llm = Llama(model_path=model_path, n_thread=8, verbose=False) #adjust for cpu cores, change as needed
+llm = Llama(model_path=model_path, n_thread=8, verbose=False, flash_attn=True) #adjust for cpu cores, change as needed
 
-prompt = "Explain photosynthesis in relation to how it is a complex process." #sample prompt
-response = generate_until_stop(llm, prompt) #generate and print to terminal
+# prompt = "Explain photosynthesis in relation to how it is a complex process." #sample prompt
+# response = generate_until_stop(llm, prompt) #generate and print to terminal
 
 def generate_quiz(llm, topic, num_questions=5):
-    raise NotImplementedError
-    return
+    #creates a prompt featuring a topic, using JSON format for easy access to questions
+    TF = randint(0, num_questions//2) # generate a random number of T/F questions, only up to half of the total number of questions
+    prompt = f"""
+    Create {TF} True/False questions, and {num_questions-TF} multiple-choice questions on the topic "{topic}" in **valid JSON**.
+    Output only {num_questions} in JSON. Do not include explanations, instructions, or extra text.
+    The JSON should be structured as:
+    {{
+        "quiz" : [
+            {{
+                "question" : "..."
+                "options" : "..."
+                "answer" : "..."
+            }},
+            {{
+                "question" : "..."
+                "options" : "..."
+                "answer" : "..."
+            }},
+            ...
+        ]
+    }}
+    """
+
+    response = llm(prompt, max_tokens=500)
+
+    return response
