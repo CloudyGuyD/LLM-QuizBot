67114cd30cb61aaccdacd8b703aa6f5dde3085c4
Tommy Duong <tommyduong588@gmail.com>
2025-08-15 22:05:31 -0700

    added loading functions for pdf and .txt files

--- rag_utils.py
+++ rag_utils.py
@@ -1,4 +1,39 @@
 from sentence_transformers import SentenceTransformer, util
+import fitz #PyMuPDF is labeled as fitz
+import os
+
+def extract_text_pdf(pdf_path): #extract the text from a .pdf
+    full_text = "" #to store text
+    try:
+        with fitz.open(pdf_path) as doc: #open the pdf
+            for page in doc:  # iterate over all pages and add the text
+                full_text += page.get_text()
+    except Exception as e:
+        print(f"Error reading PDF {pdf_path}: {e}")
+    return full_text
+
+def extract_text_txt(txt_path): #extracts text from a .txt file
+    try:
+        with open(txt_path, 'r', encoding="utf-8") as doc: #open the pdf
+            full_text = doc.read()
+    except Exception as e:
+        print(f"Error reading .txt file {txt_path}: {e}")
+    return full_text
+
+    
+
+def load_text(file_path):
+    file_extracts = {
+        ".pdf" : extract_text_pdf,
+        ".txt" : extract_text_txt
+    }
+    __, extension = os.splitext(file_path) # grab the file extension
+    extension = extension.lower() # use lowercase for consistency
+    if extension not in file_extracts.keys(): # check if file is not a pdf or txt
+        raise ValueError(f"Unsupported file type: {extension}")
+    extract = file_extracts[extension] # grab correct function for opening file
+    text = extract(file_path) # collect text
+    return text
 
 def chunk_text(text, chunk_size=300): # returns a list of strings
     words = text.split() #create a list of words
